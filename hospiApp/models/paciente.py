32265cd39af7a19fb03c133797e076baf0dca762
fdoduran89 <fdoduran1989@gmail.com>
2022-09-13 08:36:52 -0500

    arreglo psalud y paciente

--- hospiApp/models/paciente.py
+++ hospiApp/models/paciente.py
@@ -3,7 +3,7 @@ from .user import User
 from .psalud import Personalsalud
 
 class Pacientes (models.Model):
-    id_paciente = models.BigAutoField(primary_key=True)
+    id_paciente = models.AutoField(primary_key=True)
     psalud_id = models.ForeignKey(Personalsalud,related_name='paciente', on_delete=models.CASCADE)
     username= models.ForeignKey(User,related_name='paciente', on_delete=models.CASCADE)
     city = models.CharField('Ciudad',max_length=15)
