e9b2c2882083ab4fbdaf8e316ed4a5580fa6391f
fdoduran89 <fdoduran1989@gmail.com>
2022-09-14 21:15:58 -0500

    commit final

--- hospiApp/views/consulpaciente.py
+++ hospiApp/views/consulpaciente.py
@@ -2,11 +2,17 @@ from django.conf import settings
 from rest_framework import generics, status
 from hospiApp.models.paciente import Pacientes
 from hospiApp.serializers.pacienteSerializer import PacienteSerializer
+from rest_framework.response import Response
 
 class ConsulPaciente(generics.ListAPIView):
     serializer_class = PacienteSerializer
 
-    def get_queryset(self):
+    def get(self,request, pk, format=None):
+        modelo = Pacientes.objects.get(pk=pk)
+        serializer = PacienteSerializer(modelo)
+        return Response(serializer.data)
+
+    '''def get_queryset(self):
         """
         Optionally restricts the returned purchases to a given user,
         by filtering against a `username` query parameter in the URL.
@@ -15,4 +21,4 @@ class ConsulPaciente(generics.ListAPIView):
         id_paciente = self.request.query_params.get('id_paciente')
         if id_paciente is not None:
             queryset = queryset.filter(paciente__id_paciente="1")
-        return queryset
+        return queryset'''
